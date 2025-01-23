e4a915eeeb28bdb9c5b892fcb360b0fbf497a378
rohi <rohi@rgho.de>
2025-01-23 17:35:17 +0100

    sign up pages form completed

--- rgho_inventory_management/rgho_inventory_management/settings.py
+++ rgho_inventory_management/rgho_inventory_management/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    "crispy_bootstrap5",
     'inventory',
 ]
 
@@ -70,6 +72,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = 'rgho_inventory_management.wsgi.application'
 
