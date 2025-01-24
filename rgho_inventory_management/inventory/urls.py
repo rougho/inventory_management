71680e82a03dfd070a5e012d9dfb84618c239a95
rohi <rohi@rgho.de>
2025-01-24 12:43:11 +0100

    item inventory and category done

--- rgho_inventory_management/inventory/urls.py
+++ rgho_inventory_management/inventory/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Index, SignUp
+from .views import Index, SignUp, Dashboard
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path('signup/', SignUp.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
+    path('dashboard/', Dashboard.as_view(), name='dashboard'),
+
 ]
--- rgho_inventory_management/inventory/views.py
+++ rgho_inventory_management/inventory/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, View
 from .forms import UserRegisterForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import UserRegisterForm
+from .models import InventoryItem, Category
+# from inventory_management.settings import LOW_QUANTITY
+from django.contrib import messages
 
 class Index(TemplateView):
     template_name= 'inventory/index.html'
@@ -23,3 +28,31 @@ class SignUp(View):
             login(request, user)
             return redirect('index')
         return render(request, 'inventory/signup.html', {'form':form})
+    
+class Dashboard(LoginRequiredMixin, View):
+	def get(self, request):
+		items = InventoryItem.objects.filter(user=self.request.user.id).order_by('id')
+
+		low_inventory = InventoryItem.objects.filter(
+			user=self.request.user.id,
+			# quantity__lte=LOW_QUANTITY
+		)
+
+		if low_inventory.count() > 0:
+			if low_inventory.count() > 1:
+				messages.error(request, f'{low_inventory.count()} items have low inventory')
+			else:
+				messages.error(request, f'{low_inventory.count()} item has low inventory')
+
+		low_inventory_ids = InventoryItem.objects.filter(
+			user=self.request.user.id,
+			# quantity__lte=LOW_QUANTITY
+		).values_list('id', flat=True)
+
+		return render(request, 'inventory/dashboard.html', {'items': items, 'low_inventory_ids': low_inventory_ids})
+
+class Availability(View):
+      def get(self, request):
+        items = InventoryItem.objects.order_by('id')
+        return render(request, 'inventory/index.html', {'items': items})
+
